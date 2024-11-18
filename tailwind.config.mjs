1620ffc6adbe03263b0f0eb0400a505e2e5b547b
Diego Fernández <judifede217@gmail.com>
2024-11-18 18:10:51 +0000

    Prettier Revisar iconos Animaciones slide-in-down on y off LinkButton clipboardMode Toast Tailwind con class y desde window

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,33 @@ export default {
 	darkMode: 'class',
 	theme: {
 		extend: {
-			colors: {
-				
+			keyframes: {
+				'slide-in-down-on': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(-100%)'
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)'
+					}
+				},
+				'slide-in-down-off': {
+					'0%': {
+						opacity: '1',
+						transform: 'translateY(0)'
+					},
+					'100%': {
+						opacity: '0',
+						transform: 'translateY(-100%)'
+					}
+				}
+			},
+			animation: {
+				'slide-in-down-on': 'slide-in-down-on 0.5s ease-out backwards',
+				'slide-in-down-off': 'slide-in-down-off 0.5s ease-out both',
 			}
-		},
+		}
 	},
 	plugins: [],
 }
